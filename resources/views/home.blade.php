bf74f51b03bb740319075ed619e81d7455554def
MaxPredathor <max.gilli98@gmail.com>
2023-12-18 17:07:27 +0100

    style fixes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
         <div class="container d-flex">
             <div class="row my-5">
                 @foreach ( config('comics.key') as $comic)
-                    <div class="my-card col-12 col-md-4 col-lg-3">
+                    <div class="my-card col-12 col-md-4 col-lg-2">
                         <img src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }}">
                         <h3>{{ $comic['series'] }}</h3>
                     </div>
