8ea4084ba62c3e199f2689976430be966e4adf21
MaxPredathor <max.gilli98@gmail.com>
2023-12-18 15:01:52 +0100

    scaffolding + header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,23 @@
-<div>Header <i class="fa-solid fa-home"></i></div>
+<header>
+    <div class="container">
+        <div class="d-flex justify-content-between align-items-center">
+            <div class="py-3">
+                <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="DC logo">
+            </div>
+            <div class="my-div">
+                <ul class="d-flex justify-content-between">
+                    <li>CHARACTERS</li>
+                    <li>COMICS</li>
+                    <li>MOVIES</li>
+                    <li>TV</li>
+                    <li>GAMES</li>
+                    <li>COLLECTIBLES</li>
+                    <li>VIDEO</li>
+                    <li>FANS</li>
+                    <li>NEWS</li>
+                    <li>SHOP</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
